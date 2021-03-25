049c3cadbf87ff3d67bba54aef4a6009cc7e3d6c
rfsants001 <rf.sants001@gmail.com>
2021-03-24 23:07:57 -0300

    Modal & Forms

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 import { App } from './App';
 
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Venda de app',
+          type: 'deposit',
+          category: 'Desenvolvimento',
+          amount: 6000,
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: 'Luz',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 500,
+          createdAt: new Date()
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: "Transaction 1",
-          amount: 400,
-          category: 'Food',
-          type: 'deposit',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) =>{
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
